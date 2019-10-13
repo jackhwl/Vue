6da2b0c0e8489b212f371dc5ad1612ba7548f95e
Wenlin Huang <jackhwl@hotmail.com>
2019-10-13 13:56:24 -0400

    with array

--- vue-next-master/1.vue2.0.js
+++ vue-next-master/1.vue2.0.js
@@ -1,13 +1,29 @@
 // Vue 2. how to impliment reactive
 // update view when data change
-
+let oldArrayPrototype = Array.prototype;
+let proto = Object.create(oldArrayPrototype); // inherit
+['push', 'shift', 'unshift'].forEach(method => {
+     proto[method] = function() { // hijack function, rewrite function, internal continue call old method 
+        updateView(); //slice programming;
+        oldArrayPrototype[method].call(this, ...arguments);
+     }
+})
 function observer(target) {
     if (typeof target !== 'object' || target === null){
         return target;
     }
-    for (let key in target) {
-        defineReactive(target, key, target[key]);
+    if (Array.isArray(target)){ // hijack array, rewrite method in array
+        Object.setPrototypeOf(target, proto); 
+        //target.__proto__ = proto;
+        for(let i=0; i<target.length; i++) {
+            observer(target[i]);
+        }
+    } else {
+        for (let key in target) {
+            defineReactive(target, key, target[key]);
+        }
     }
+
 }
 
 function defineReactive(target, key, value){
@@ -30,10 +46,15 @@ function updateView() {
     console.log('update view');
 }
 
-let data = {name: 'jack', age: {n:100}};
-observer(data);
+//let data = {name: 'jack', age: {n:100}};
+//observer(data);
 //data.name = 'wenlin';
 //data.age.n = 200;
-data.age = {n:200};
-data.age.n = 300;
-console.log(data.name);
+// data.age = {n:200};
+// data.age.n = 300;
+//console.log(data.name);
+
+let data = {name: 'jack', age: [1,2,3]};
+observer(data);
+data.age.push(4); 
+console.log(data.age);
