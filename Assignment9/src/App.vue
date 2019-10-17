e3c4c73309656406bd3f89a8ac409a9164e22df8
Wenlin Huang <jackhwl@hotmail.com>
2019-10-17 19:07:57 -0400

    Assignment: Time to Practice - Slots and Dynamic Components

---
+++ Assignment9/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="selectedComponent='appBlue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="selectedComponent='appGreen'" class="btn btn-success">Load Green Template</button>
+                <button @click="selectedComponent='appRed'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <component :is="selectedComponent"></component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data: function(){
+            return {
+                selectedComponent: 'appBlue'
+            }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
