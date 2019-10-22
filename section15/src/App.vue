30f0675e1e112f753b607d85648eb070011f23ee
Wenlin Huang <jackhwl@hotmail.com>
2019-10-22 19:36:26 -0400

    224. Intercepting Responses

--- section15/src/App.vue
+++ section15/src/App.vue
@@ -35,7 +35,7 @@
         },
         methods: {
             submit() {
-                this.$http.post('https://udemyvue2.firebaseio.com/data.json', this.user)
+                this.$http.post('', this.user)
                 .then(response=> {
                     console.log(response)
                 }, error=> {
@@ -43,7 +43,7 @@
                 });
             },
             fetchData() {
-                this.$http.get('https://udemyvue2.firebaseio.com/data.json')
+                this.$http.get('')
                 .then(response=>{
                     return response.json();
                 })
--- section15/src/main.js
+++ section15/src/main.js
@@ -4,6 +4,16 @@ import App from './App.vue'
 
 Vue.use(VueResource);
 
+Vue.http.options.root = 'https://udemyvue2.firebaseio.com/data.json';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method == 'POST') {
+    request.method = 'PUT';
+  }
+  next(response => {
+    response.json = () => { return {messages: response.body}};
+  });
+})
 new Vue({
   el: '#app',
   render: h => h(App)
