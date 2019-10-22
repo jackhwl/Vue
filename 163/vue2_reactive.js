ea6c9fbc47f6a025fa89cf0d56222a5446a1405e
Wenlin Huang <jackhwl@hotmail.com>
2019-10-21 20:36:24 -0400

    Vue2.0 reactive handle array

--- 163/vue2_reactive.js
+++ 163/vue2_reactive.js
@@ -8,28 +8,49 @@ function vi2() {
     this.observe(this.$data);
     this.render();
 }
-
+let oldArrayPotoType = Array.prototype;
+let proto = Object.create(oldArrayPotoType);
+['push', 'shift','unshift'].forEach(method => {
+    proto[method] = function() {
+        //updateView();
+        oldArrayPotoType[method].call(this, ...arguments);
+    }
+});
 vi2.prototype.observe = function(obj){
-    var value;
-    var self = this;
-    for(var key in obj) {
-        value = obj[key];
-        if (typeof value === 'object') {
-            this.observe(value);
-        } else {
-            Object.defineProperty(obj, key, {
-                get: function(){
-                    return value;
-                },
-                set: function(newValue){
-                    value = newValue;
-                    self.render();
-                } 
-            })
+    if (typeof obj !== 'object' || obj == null) {
+        return obj;
+    }
+    if (Array.isArray(obj)){
+        Object.setPrototypeOf(obj,proto);
+        //obj.__proto__ = proto;
+        for(let i=0;i<obj.length;i++){
+            this.observe(obj[i]);
+        }
+    } else {
+        for(var key in obj) {
+            defineReactive(obj, key, obj[key], self);
         }
     }
 }
 
+function defineReactive(obj, key, value, self){
+    //if (typeof value === 'object') {
+        self.observe(value);
+    //} else {
+        Object.defineProperty(obj, key, {
+            get: function(){
+                return value;
+            },
+            set: function(newValue){
+                if(newValue !== value){
+                    self.observe(newValue);
+                    value = newValue;
+                    self.render();
+                }
+            } 
+        })
+    //}
+}
 vi2.prototype.render = function(){
     this._html="I am " + this.$data.a + " and a's child:" + this.$data.foo.bar;
     this.el.innerHTML= this._html;
