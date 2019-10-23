816faa0ff0a73089ff3d7c9c468bd05af5c6d5c9
Wenlin Huang <jackhwl@hotmail.com>
2019-10-23 08:37:34 -0400

    241. vue-router 2.2:Extract Route Params via props

--- Section16/src/components/user/User.vue
+++ Section16/src/components/user/User.vue
@@ -1,4 +1,27 @@
 <template>
-    <h1>The User Page</h1>
-
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <p>Loadded ID: {{id}} </p>
+        <button @click="navigateToHome">Go to Home</button>
+    </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    },
+    watch: {
+        '$route'(to, from){
+            this.id = to.params.id;
+        }
+    },
+    methods: {
+        navigateToHome() {
+            this.$router.push('/');
+        }
+    }
+}
+</script>
