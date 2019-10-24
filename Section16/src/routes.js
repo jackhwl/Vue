a534bf1703c534c438d95734f32ca5abfd8e433f
Wenlin Huang <jackhwl@hotmail.com>
2019-10-23 20:53:40 -0400

    257. Loading Routes Lazily

--- Section16/src/routes.js
+++ Section16/src/routes.js
@@ -1,9 +1,27 @@
-import User from './components/user/User.vue';
-import UserStart from './components/user/UserStart.vue';
-import UserDetail from './components/user/UserDetail.vue';
-import UserEdit from './components/user/UserEdit.vue';
+
 import Home from './components/Home.vue';
 import Header from './components/Header.vue';
+
+const User = resolve => {
+    require.ensure(['./components/user/User.vue'], () => {
+        resolve(require('./components/user/User.vue'));
+    });
+};
+const UserStart = resolve => {
+    require.ensure(['./components/user/UserStart.vue'], () => {
+        resolve(require('./components/user/UserStart.vue'));
+    });
+};
+const UserEdit = resolve => {
+    require.ensure(['./components/user/UserEdit.vue'], () => {
+        resolve(require('./components/user/UserEdit.vue'));
+    });
+};
+const UserDetail = resolve => {
+    require.ensure(['./components/user/UserDetail.vue'], () => {
+        resolve(require('./components/user/UserDetail.vue'));
+    });
+};
 
 export const routes = [
     { path: '', name: 'home', components: {
