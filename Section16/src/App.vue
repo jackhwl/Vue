5487d93d8e8bcf26c89671e2e0e01d3cd268c968
Wenlin Huang <jackhwl@hotmail.com>
2019-10-23 20:10:48 -0400

    253. Controlling the Scroll Behavior

--- Section16/src/App.vue
+++ Section16/src/App.vue
@@ -4,8 +4,11 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
                 <hr>
-                <app-header></app-header>
-                <router-view></router-view>
+                <router-view name="header-top"></router-view>
+                <transition name="slide" mode="out-in">
+                    <router-view></router-view>
+                </transition>
+                <router-view name="header-bottom"></router-view>
             </div>
         </div>
     </div>
@@ -21,4 +24,36 @@ import Header from './components/Header.vue';
 </script>
 
 <style>
+.slide-leave-active {
+        transition: opacity 1s ease;
+        opacity: 0;
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-30px);
+        }
+    }
+
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-30px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
 </style>
