3631dbcf53b4d34074df3f4fcdb158c3faabeeb4
Wenlin Huang <jackhwl@hotmail.com>
2019-10-30 08:09:32 -0400

    8. Data Binding

--- vue-firebase/app.js
+++ vue-firebase/app.js
@@ -1,12 +1,14 @@
 new Vue({
-    el: '#app',
-    data: {
-      title: 'Becoming a Vue ninja',
-      name: 'Ryu'
-    },
-    methods: {
-      greet(time){
-        return `Hello and good ${time}, ${this.name}`
-      }
+  el: '#app',
+  data: {
+    title: 'Becoming a Vue ninja',
+    name: 'Ryu',
+    url: 'http://www.youtube.com',
+    classes: ['one', 'two']
+  },
+  methods: {
+    greet(time){
+      return `Hello and good ${time}, ${this.name}`
     }
-  })
+  }
+})
