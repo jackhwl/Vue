50533e2946907f4e9eea187c98db59972b58da19
Wenlin Huang <jackhwl@hotmail.com>
2019-11-01 07:45:34 -0400

    15. Looping with v-for

--- vue-firebase/app.js
+++ vue-firebase/app.js
@@ -3,7 +3,13 @@ new Vue({
   data: {
     title: 'Becoming a Vue ninja',
     name: 'Ryu',
-    showName: true
+    showName: true,
+    items: ["aaa", "bbb", "ccc"],
+    ninjas: [
+      { name: 'Crystal', age: 25, belt: 'Black'},
+      { name: 'Ryu', age: 30, belt: 'Brown'},
+      { name: 'Ken', age: 35, belt: 'Orange'} 
+    ]
   },
   methods: {
     updateName(e){
