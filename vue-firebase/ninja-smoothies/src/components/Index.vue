6629cc672a8bd2fc87b353b664c6d74c14076e28
Wenlin Huang <jackhwl@hotmail.com>
2019-11-02 16:28:44 -0400

    41. Deleting (local) Data

--- vue-firebase/ninja-smoothies/src/components/Index.vue
+++ vue-firebase/ninja-smoothies/src/components/Index.vue
@@ -2,6 +2,7 @@
   <div class="index container">
     <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
       <div class="card-content">
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
         <h2 class="indigo-text">{{ smoothie.title }}</h2>
         <ul class="ingredients">
           <li v-for="(ing, index) in smoothie.ingredients" :key="index">
@@ -22,6 +23,11 @@ export default {
         { title: 'Ninja Brew', slug: 'ninja-brew', ingredients: ['bananas', 'coffee', 'milk'], id: '1'},
         { title: 'Morning Mood', slug: 'morning-mood', ingredients: ['mango', 'lime', 'juice'], id: '2'}
       ]
+    }
+  },
+  methods: {
+    deleteSmoothie(id) {
+      this.smoothies = this.smoothies.filter(s => s.id !== id)
     }
   }
 }
@@ -46,5 +52,12 @@ export default {
 .index .ingredients li{
   display: inline-block;
 }
-
+.index .delete{
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.4em;
+}
 </style>
