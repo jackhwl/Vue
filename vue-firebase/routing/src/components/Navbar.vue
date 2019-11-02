f79a9d00635968b3ef3b3b60f2e8c48f6eb2db00
Wenlin Huang <jackhwl@hotmail.com>
2019-11-02 13:51:43 -0400

    32. More on Router Links

--- vue-firebase/routing/src/components/Navbar.vue
+++ vue-firebase/routing/src/components/Navbar.vue
@@ -4,6 +4,14 @@
             <li><router-link to="/">Home</router-link></li>
             <li><router-link :to="{name: 'About'}">About</router-link></li>
         </ul>
+        <h2>User Pofiles</h2>
+        <ul>
+            <li v-for="(id, index) in userIds" :key="index">
+                <router-link :to="{name: 'ViewProfile', params: { user_id: id }}">
+                    <span>Profile {{id}}</span>
+                </router-link>
+            </li>
+        </ul>
     </nav>
 </template>
 
@@ -12,7 +20,7 @@ export default {
     name: 'Navbar',
     data() {
         return {
-
+            userIds: ['1','2','3','4']
         }
     }
 }
