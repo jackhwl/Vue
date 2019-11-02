53bb0b32693831a11e4ddac5533430e5ab7eb3cf
Wenlin Huang <jackhwl@hotmail.com>
2019-11-02 18:15:22 -0400

    44. Installing Firebase

---
+++ vue-firebase/ninja-smoothies/src/firebase/init.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+apiKey: "AIzaSyAzK2S9W9w7Vm0eaitE8QXUkOj7AgrNxWY",
+authDomain: "udemy-ninja-smoothies-75ec1.firebaseapp.com",
+databaseURL: "https://udemy-ninja-smoothies-75ec1.firebaseio.com",
+projectId: "udemy-ninja-smoothies-75ec1",
+storageBucket: "udemy-ninja-smoothies-75ec1.appspot.com",
+messagingSenderId: "672621756712",
+appId: "1:672621756712:web:7de17f2388bb89728d101d",
+measurementId: "G-06VLX4NKHX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebaseApp.firestore().settings({ timestampsInSnapshots: true })
+
+// export firestore database
+export default firebaseApp.firestore()
