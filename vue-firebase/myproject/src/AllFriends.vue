78cfddcfd73851d4f52a71d59dab6b9008e3c39c
Wenlin Huang <jackhwl@hotmail.com>
2019-11-01 20:05:02 -0400

    22. Custom Events

--- vue-firebase/myproject/src/AllFriends.vue
+++ vue-firebase/myproject/src/AllFriends.vue
@@ -2,7 +2,7 @@
     <div id="all-friends">
         <h2>All Friends</h2>
         <div v-for="(friend, index) in friends" :key="index">
-            <span>{{ friend.name }}</span>
+            <span @click="unfriend(friend.name)">{{ friend.name }}</span>
         </div>
     </div>
 </template>
@@ -13,6 +13,11 @@ export default {
     name: 'AllFriends',
     data() {
         return {
+        }
+    },
+    methods: {
+        unfriend(name) {
+            this.$emit('delete', {name})
         }
     }
 }
