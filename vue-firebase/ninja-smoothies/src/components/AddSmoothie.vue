64cf0a97cbed99ebf35e7c8a9dc2d6147d83d494
Wenlin Huang <jackhwl@hotmail.com>
2019-11-03 08:44:22 -0500

    49. Outputting Ingredients

---
+++ vue-firebase/ninja-smoothies/src/components/AddSmoothie.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="add-smoothie container">
+        <h2 class="center-aline indigo-text">Add New Smoothie Recipe</h2>
+        <form @submit.prevent="AddSmoothie">
+            <div class="field title">
+                <label for="title">Smoothie Title:</label>
+                <input type="text" name="title" v-model="title">
+            </div>
+            <div v-for="(ing, index) in ingredients" :key="index">
+                <label for="ingredient">Ingredient:</label>
+                <input type="text" name="ingredient" v-model="ingredients[index]">
+            </div>
+            <div class="field add-ingredient">
+                <label for="add-ingredient">Add an ingredient:</label>
+                <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
+            </div>
+            <div class="field center-align">
+                <p v-if="feedback" class="red-text">{{ feedback }}</p>
+                <button class="btn pink">Add Smoothie</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddSmoothie',
+    data(){
+        return {
+            title: null,
+            another: null,
+            ingredients: [],
+            feedback: null
+        }
+    },
+    methods: {
+        AddSmoothie(){
+            console.log(this.title, this.ingredients)
+        },
+        addIng(){
+            if (this.another) {
+                this.ingredients.push(this.another)
+                this.another = null
+                this.feedback = null
+            } else {
+                this.feedback = 'You must enter a value to add an ingredient'
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.add-smoothie{
+    margin-top: 60px;
+    padding: 20px;
+    max-width: 500px;
+}
+.add-smoothie h2{
+    font-size: 2em;
+    margin:20px auto;
+}
+.add-smoothie .field{
+    margin: 20px auto;
+}
+</style>
