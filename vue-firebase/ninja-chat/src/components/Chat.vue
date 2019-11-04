d2abb83afcb7c812d553712084a97224de96a8d6
Wenlin Huang <jackhwl@hotmail.com>
2019-11-04 08:22:59 -0500

    65. New Message Component

--- vue-firebase/ninja-chat/src/components/Chat.vue
+++ vue-firebase/ninja-chat/src/components/Chat.vue
@@ -1,10 +1,26 @@
 <template>
     <div class="chat container">
-        <h2>Chat {{ this.name }}</h2>
+        <h2 class="center teal-text">Ninja Chat</h2>
+        <div class="card">
+            <div class="card-content">
+                <ul class="messages">
+                    <li>
+                        <span class="teal-text">Name</span>
+                        <span class="grey-text text-darken-3">message</span>
+                        <span class="grey-text time">time</span>
+                    </li>
+                </ul>
+            </div>    
+        </div>
+        <div class="card-action">
+            <NewMessage :name="name"/>
+        </div>
     </div>
 </template>
 
 <script>
+import NewMessage from '@/components/NewMessage'
+
 export default {
     name: 'Chat',
     props: ['name'],
@@ -12,6 +28,22 @@ export default {
         return {
 
         }
+    },
+    components: {
+        NewMessage
     }
 }
 </script>
+<style>
+.chat h2{
+    font-size: 2.6em;
+    margin-bottom: 40px;
+}
+.chat span{
+    font-size: 1.4em;
+}
+.chat .time{
+    display: block;
+    font-size: 1.2em;
+}
+</style>
