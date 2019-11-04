ba7b0c2e1f4f934ce05f1e22fa55f90bb5610047
Wenlin Huang <jackhwl@hotmail.com>
2019-11-04 08:04:17 -0500

    62. Passing Props Via Routes

--- vue-firebase/ninja-chat/src/router/index.js
+++ vue-firebase/ninja-chat/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Welcome from '@/components/Welcome'
+import Chat from '@/components/Chat'
 
 Vue.use(Router)
 
@@ -8,8 +9,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Welcome',
+      component: Welcome
+    },
+    {
+      path: '/chat',
+      name: 'Chat',
+      component: Chat,
+      props: true
     }
   ]
 })
