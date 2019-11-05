ccfe16b59b9e84a5bf3dc21da9c785087121bd26
Wenlin Huang <jackhwl@hotmail.com>
2019-11-04 20:48:14 -0500

    67. Real-Time Events (event listeners)

--- vue-firebase/ninja-chat/src/components/NewMessage.vue
+++ vue-firebase/ninja-chat/src/components/NewMessage.vue
@@ -3,22 +3,36 @@
         <form @submit.prevent="addMessage">
             <label for="new-message">New Message (enter to add):</label>
             <input type="text" name="new-message" v-model="newMessage">
+            <p class="red-text" v-if="feedback">{{feedback}}</p>
         </form>
     </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
+
 export default {
     name: 'NewMessage',
     props: ['name'],
     data() {
         return {
-            newMessage: null
+            newMessage: null,
+            feedback: null
         }
     },
     methods: {
         addMessage() {
-            console.log(this.newMessage, this.name, Date.now())
+            if (this.newMessage) {
+                db.collection('message').add({
+                    content: this.newMessage,
+                    name: this.name,
+                    timestamp: Date.now()
+                }).catch(err => console.error(err))
+                this.newMessage = null
+                this.feedback = null
+            } else {
+                 this.feedback = "You must enter a message in order to send one"
+            }
         }
     }
 }
