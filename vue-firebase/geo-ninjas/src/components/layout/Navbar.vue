95f098aa5557c9b1f2159c1ebad1dfe00535001c
Wenlin Huang <jackhwl@hotmail.com>
2019-11-06 07:53:22 -0500

    93. User Profiles

--- vue-firebase/geo-ninjas/src/components/layout/Navbar.vue
+++ vue-firebase/geo-ninjas/src/components/layout/Navbar.vue
@@ -2,11 +2,12 @@
     <div class="navbar">
         <nav class="deep-purple darken-1">
             <div class="container">
-                <router-link :to="{name: 'GMap'}">GeoNinjas!</router-link>
+                <router-link :to="{name: 'GMap'}" class="brand-logo left">GeoNinjas!</router-link>
                 <ul class="right">
-                    <li><router-link :to="{name: 'Signup'}">Signup</router-link></li>
-                    <li><router-link :to="{name: 'Login'}">Login</router-link></li>
-                    <li><a @click="logout">Logout</a></li>
+                    <li v-if="!user"><router-link :to="{name: 'Signup'}">Signup</router-link></li>
+                    <li v-if="!user"><router-link :to="{name: 'Login'}">Login</router-link></li>
+                    <li v-if="user"><a>{{user.email}}</a></li>
+                    <li v-if="user"><a @click="logout">Logout</a></li>
                 </ul>
             </div>
         </nav>
@@ -20,7 +21,7 @@ export default {
     name: 'Navbar',
     data() {
         return {
-
+            user: null
         }
     },
     methods: {
@@ -29,6 +30,15 @@ export default {
                 this.$router.push({name: 'Login'})
             })
         }
+    },
+    created() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user){
+                this.user = user
+            } else {
+                this.user = null
+            }
+        })
     }
 }
 </script>
---
+++ vue-firebase/geo-ninjas/src/components/profile/ViewProfile.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="view-profile container">
+        <div class="card" v-if="profile">
+            <h2 class="deep-purple-text center">{{ profile.alias }}'s Wall</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+import db from '@/firebase/init'
+
+export default {
+    data() {
+        return {
+            profile: null
+        }
+    },
+    created() {
+        let ref = db.collection('users')
+        ref.doc(this.$route.params.id).get()
+        .then(user => {
+            this.profile = user.data()
+        })
+    }
+}
+</script>
