5af0021b514695a7fe9601f5930beff515c27d42
Wenlin Huang <jackhwl@hotmail.com>
2020-09-24 19:33:32 -0400

    docs

---
+++ Introduction to Vue 3/ex1.js
@@ -0,0 +1,16 @@
+// https://codepen.io/jackhwl/pen/dyMwBXp
+
+const App = {
+    data() {
+      return {
+        num1: 3,
+        num2: 4,
+        name: ''
+      }
+    }
+  }
+  
+  Vue.createApp(App).mount('#app')
+  
+  
+  
---
+++ Introduction to Vue 3/readme2.md
@@ -0,0 +1,7 @@
+
+https://kentcdodds.com/blog/use-ternaries-rather-than-and-and-in-jsx
+
+https://www.smashingmagazine.com/2018/02/jquery-vue-javascript/
+
+https://slides.com/sdrasner/intro-to-vue-3-3?token=NLsRwMvr#/47
+!vue!
